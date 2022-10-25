a7e6f909ba8751fb931aa32a5a8ff4dc0b8dad55
Henrique-Botelho <henriquebotelho.imp@gmail.com>
2022-10-25 10:54:03 -0300

    mudança Home
    tirei a importação da Image

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {View, Text, StyleSheet, ImageBackground, TouchableOpacity, Image} from "react-native";
+import {View, Text, StyleSheet, ImageBackground, TouchableOpacity} from "react-native";
 
 export default function Home(props) {
     return(
