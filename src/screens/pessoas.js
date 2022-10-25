acb5e046949114f823cf46742882411b5cadd53e
heitor <you@example.com>
2022-10-25 10:26:11 -0300

    página pessoas atualizada

--- src/screens/pessoas.js
+++ src/screens/pessoas.js
@@ -1,10 +1,15 @@
 import React from "react";
 import {View, Text, StyleSheet} from "react-native";
+import SwitchButton from "../components/switch";
 
 export default function Pessoas() {
     return(
         <View>
             <Text>Primeira tela</Text>
+            <View style={styles.opcoes}>
+                <Text>Há pessoas vegetarianas?</Text><SwitchButton />
+                <Text>Há pessoas veganas?</Text><SwitchButton />
+            </View>
         </View>
     );
 }
