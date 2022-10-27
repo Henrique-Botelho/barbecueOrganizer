e0fa382d26204c2a9ee5dd02f802a4a725f0ff15
Henrique-Botelho <henriquebotelho.imp@gmail.com>
2022-10-27 08:41:20 -0300

    Update mainContext.js

--- src/context/mainContext.js
+++ src/context/mainContext.js
@@ -5,13 +5,29 @@ export const MainContext = createContext();
 
 export default function AuthProvider({children}){
 
+    // const [dados, setDados] = useState();
+
+    // const getDados = async () => {
+    //     const response = await data;
+    //     setDados(response);
+    // }
+    // getDados();
+
     const [dados, setDados] = useState();
 
-    const getDados = async () => {
-        const response = await data;
-        setDados(response);
-    }
-    getDados();
+    useEffect(() => {
+        const getData = () => {
+            return new Promise((resolve, reject) => {
+                resolve(data)
+                reject("Não foi possível acessar os dados.")
+            }) 
+        }
+
+        getData()
+            .then(value => {setDados(JSON.stringify(value))})
+            .catch(error => console.log(error));
+
+    }, []); 
     
     return(
         <MainContext.Provider value={{dados}}>
@@ -19,19 +35,4 @@ export default function AuthProvider({children}){
         </MainContext.Provider>
     );
 
-    // const [dados, setDados] = useState();
-
-    // useEffect(() => {
-    //     const getData = () => {
-    //         return new Promise((resolve, reject) => {
-    //             resolve(data)
-    //             reject("Não foi possível acessar os dados.")
-    //         }) 
-    //     }
-
-    //     getData()
-    //         .then(value => {setDados(JSON.stringify(value))})
-    //         .catch(error => console.log(error));
-
-    // }, []); 
 }
