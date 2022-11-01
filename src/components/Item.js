e694c1ca95506012beb7fde608c45b6102d72fcd
Henrique-Botelho <henriquebotelho.imp@gmail.com>
2022-11-01 10:37:25 -0300

    Update Item.js

--- src/components/Item.js
+++ src/components/Item.js
@@ -2,19 +2,20 @@ import React, { useContext, useState } from "react";
 import { View, TouchableOpacity, Text } from "react-native";
 import { MainContext } from "../context/mainContext";
 import BouncyCheckbox from "react-native-bouncy-checkbox";
-import CheckBox from "@react-native-community/checkbox";
 
 export default function Item(props) {
     const {setItem, data} = useContext(MainContext);
-    let estado = data.comidas[props.class][props.position].status;
-    const [checkBoxState, setCheckBoxState] = useState(estado);
+    const [checkBoxState, setCheckBoxState] = useState(data.comidas[props.class][props.position].status);
     
     setItem(props.class, props.position, checkBoxState);
 
+    // console.log(`${props.name}:`, data.comidas[props.class][props.position].status)
+
     return (
-        <TouchableOpacity onPress={() => {setCheckBoxState(!checkBoxState); console.log(checkBoxState)}}>
+        <TouchableOpacity onPress={() => {setCheckBoxState(!checkBoxState)}}>
             <BouncyCheckbox disableBuiltInState={true} isChecked={checkBoxState} onPress={() => {setCheckBoxState(!checkBoxState)}} />
             <Text>{props.name}</Text>
+            
         </TouchableOpacity>
     );
 }
