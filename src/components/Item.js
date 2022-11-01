24a998509e89ff1b4bec40f918b0ed8749f865a5
Henrique Botelho <henriquebotelho.imp@gmail.com>
2022-11-01 18:23:42 -0300

    Revert "Update Item.js"
    This reverts commit e694c1ca95506012beb7fde608c45b6102d72fcd.

--- src/components/Item.js
+++ src/components/Item.js
@@ -2,20 +2,19 @@ import React, { useContext, useState } from "react";
 import { View, TouchableOpacity, Text } from "react-native";
 import { MainContext } from "../context/mainContext";
 import BouncyCheckbox from "react-native-bouncy-checkbox";
+import CheckBox from "@react-native-community/checkbox";
 
 export default function Item(props) {
     const {setItem, data} = useContext(MainContext);
-    const [checkBoxState, setCheckBoxState] = useState(data.comidas[props.class][props.position].status);
+    let estado = data.comidas[props.class][props.position].status;
+    const [checkBoxState, setCheckBoxState] = useState(estado);
     
     setItem(props.class, props.position, checkBoxState);
 
-    // console.log(`${props.name}:`, data.comidas[props.class][props.position].status)
-
     return (
-        <TouchableOpacity onPress={() => {setCheckBoxState(!checkBoxState)}}>
+        <TouchableOpacity onPress={() => {setCheckBoxState(!checkBoxState); console.log(checkBoxState)}}>
             <BouncyCheckbox disableBuiltInState={true} isChecked={checkBoxState} onPress={() => {setCheckBoxState(!checkBoxState)}} />
             <Text>{props.name}</Text>
-            
         </TouchableOpacity>
     );
 }
