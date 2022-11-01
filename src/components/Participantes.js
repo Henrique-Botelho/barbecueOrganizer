4ec2b01d97028d7d0ae84761a1c28230ddba75bd
Henrique-Botelho <henriquebotelho.imp@gmail.com>
2022-11-01 11:55:26 -0300

    Changes

--- src/components/Participantes.js
+++ src/components/Participantes.js
@@ -1,7 +1,9 @@
 import React, { useContext, useState } from "react";
 import { View, TouchableOpacity, Text } from "react-native";
 import { MainContext } from "../context/mainContext";
-
+import { StyleSheet } from "react-native";
+import { processFontFamily } from "expo-font";
+import { transform } from "@babel/core";
 export default function Participantes (props) {
     const {adicionaPessoas, somaPessoas} = useContext(MainContext);
     const [quantidade, setQuantidade] = useState(0);
@@ -10,9 +12,9 @@ export default function Participantes (props) {
     somaPessoas();
 
     return (
-        <View>
-            <Text>{props.pessoa}</Text>
-            <View>
+        <View style={styles.convidados}>
+            <Text style={styles.genero}>{props.pessoa}</Text>
+            <View style={styles.pessoas}>
                 <TouchableOpacity onPress={() => {if (quantidade == 0) {setQuantidade(quantidade)} else {setQuantidade(quantidade - 1)}}}>
                     <Text>-</Text>
                 </TouchableOpacity>
@@ -25,3 +27,23 @@ export default function Participantes (props) {
         </View>
     );
 }
+
+const styles = StyleSheet.create ({
+    pessoas:{
+        borderRadius: 5,
+        borderWidth: 5,
+        borderColor: "blue",
+        flexDirection: "row",
+        justifyContent: "space-around"
+    },
+    genero:{
+        borderRadius: 5,
+        borderWidth: 5,
+        borderColor: "red",
+        fontSize: 20,
+        fontFamily: 'Arial',
+        textTransform: "capitalize"
+        
+
+    },
+});
