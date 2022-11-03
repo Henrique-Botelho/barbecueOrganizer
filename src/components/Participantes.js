5d04c072abb5999dee27e8aa28c95756d5b4d3ee
GuiLeoni <ws.leoni16@gmail.com>
2022-11-03 10:10:19 -0300

    Front-End Pessoas FINALIZADO

--- src/components/Participantes.js
+++ src/components/Participantes.js
@@ -1,27 +1,77 @@
 import React, { useContext, useState } from "react";
 import { View, TouchableOpacity, Text, StyleSheet } from "react-native";
+import { Directions } from "react-native-gesture-handler";
 import { MainContext } from "../context/mainContext";
 
-export default function Participantes (props) {
-    const {adicionaPessoas, somaPessoas} = useContext(MainContext);
-    const [quantidade, setQuantidade] = useState(0);
+export default function Participantes(props) {
+  const { adicionaPessoas, somaPessoas } = useContext(MainContext);
+  const [quantidade, setQuantidade] = useState(0);
 
-    adicionaPessoas(props.pessoa, quantidade);
-    somaPessoas();
+  adicionaPessoas(props.pessoa, quantidade);
+  somaPessoas();
 
-    return (
-        <View>
-            <Text>{props.pessoa}</Text>
-            <View>
-                <TouchableOpacity onPress={() => {if (quantidade == 0) {setQuantidade(quantidade)} else {setQuantidade(quantidade - 1)};}}>
-                    <Text>-</Text>
-                </TouchableOpacity>
-                <Text>{quantidade}</Text>
-                <TouchableOpacity
-                    onPress={() => {setQuantidade(quantidade + 1)}}>
-                    <Text>+</Text>
-                </TouchableOpacity>
-            </View>
-        </View>
-    );
+  return (
+    <View style={styles.genero}>
+      <Text style={styles.textgenero}>{props.pessoa}</Text>
+      <View style={styles.quantidade}>
+        <TouchableOpacity
+          onPress={() => {
+            if (quantidade == 0) {
+              setQuantidade(quantidade);
+            } else {
+              setQuantidade(quantidade - 1);
+            }
+          }}
+        >
+          <Text style={styles.botoes}>-</Text>
+        </TouchableOpacity>
+        <Text style={styles.numero}>{quantidade}</Text>
+        <TouchableOpacity
+          onPress={() => {
+            setQuantidade(quantidade + 1);
+          }}
+        >
+          <Text style={styles.botoes}>+</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+    botoes: {
+        fontSize: 25,
+    },
+    numero: {
+        fontSize: 22,
+    },
+    quantidade: {
+    borderWidth: 0.1,
+    borderColor: "black",
+    flexDirection: "row",
+    letterSpacing: 20,
+    justifyContent: "center",
+    backgroundColor: "#FFCE51",
+    borderRadius: 5,
+    marginTop: 5,
+    padding: 5,
+    width: 120,
+    marginLeft: "auto",
+    marginRight: "auto",
+    paddingLeft: 25,
+  },
+  textgenero: {
+    letterSpacing: 2,
+    fontSize: 17,
+  },
+  genero: {
+
+    justifyContent: "center",
+    backgroundColor: "#FFCE51",
+    borderRadius: 5,
+    padding: 5,
+    margim: 80,
+    textAlign: "center",
+    width: 200,
+  },
+});
