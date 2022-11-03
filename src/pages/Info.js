76bc3918d98bc180385c84707476cb12e1e8968d
Heitor <heitorsclaudino04@gmail.com>
2022-11-03 12:01:03 -0300

    Inserindo mapa

--- src/pages/Info.js
+++ src/pages/Info.js
@@ -1,6 +1,7 @@
 import React, { useContext, useState } from "react";
-import { View, Text, TextInput, StyleSheet, ImageBackground, TouchableOpacity } from "react-native";
+import { View, Text, TextInput, StyleSheet, ImageBackground, TouchableOpacity, Dimensions } from "react-native";
 import { MainContext } from "../context/mainContext";
+import MapView from "react-native-maps";
 
 export default function Info(props) {
     const {data, setInfo} = useContext(MainContext);
@@ -36,6 +37,9 @@ export default function Info(props) {
                     
                 </View>
                 <Text>Local</Text>
+                <MapView 
+                    style={styles.map}
+                />
                 <View>
                     <Text>Endereço: 
                     </Text>
@@ -92,5 +96,9 @@ const styles = StyleSheet.create({
     inputs: {
         borderColor: "#fff",
         borderWidth: 3
+    },
+    map: {
+        height: 250,
+        width: Dimensions.get('window').width,
     }
 })
