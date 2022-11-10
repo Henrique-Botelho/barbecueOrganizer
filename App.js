56c010d2ec8f253d1d545be395a766a30fb57ff8
Henrique-Botelho <henriquebotelho.imp@gmail.com>
2022-11-10 10:06:09 -0300

    Update App.js

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import React, { useEffect } from "react";
 import Routes from "./src/routes";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
+AsyncStorage.setItem("Chave 1", "Olá mundo");
+
 export default function App(){
   return <Routes />;
 }
