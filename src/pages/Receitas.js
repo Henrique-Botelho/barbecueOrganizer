2f938c50520e520b25f66e09d06984a222d81a4a
heitorsclaudino <heitorsclaudino04@gmail.com>
2022-11-17 10:24:17 -0300

    Modal receitas (quase perfeito)
    Modal receitas funcionando, problema no botão que precisa de 2 clicks para funcionar

--- src/pages/Receitas.js
+++ src/pages/Receitas.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { View, Text, StyleSheet, ImageBackground, Modal, TouchableOpacity } from "react-native";
 
 import ModalReceita from "../components/ModalReceita";
@@ -10,14 +10,69 @@ export default function Receitas(props) {
 
   return (
     <View>
-        <Text>Carne bovina</Text>
-        <TouchableOpacity onPress={() => {
-          setIsEnabled(!isEnabled);
-          setID("1");       
-        }}>
-          <Text>Contra-filé</Text>
-        </TouchableOpacity>
-        {id && isEnabled ? <ModalReceita id={id} ativo={isEnabled}/> : ""}
+        <Text style={styles.titulo}>Carne bovina</Text>
+            <TouchableOpacity onPress={() => {
+              setIsEnabled(!isEnabled);
+              setID("1");
+            }}>
+              <Text style={styles.btnTitle}>Contra-filé</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => {
+              setIsEnabled(!isEnabled);
+              setID("2");        
+            }}>
+              <Text style={styles.btnTitle}>Fraldinha</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => {
+              setIsEnabled(!isEnabled);
+              setID("3");       
+            }}>
+              <Text style={styles.btnTitle}>Maminha</Text>
+            </TouchableOpacity>
+
+
+        <Text style={styles.titulo}>Carne suína</Text>   
+            <TouchableOpacity onPress={() => {
+              setIsEnabled(!isEnabled);
+              setID("4");       
+            }}>
+              <Text style={styles.btnTitle}>Picanha Suína</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => {
+              setIsEnabled(!isEnabled);
+              setID("5");       
+            }}>
+              <Text style={styles.btnTitle}>Linguiça</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => {
+              setIsEnabled(!isEnabled);
+              setID("6");       
+            }}>
+              <Text style={styles.btnTitle}>Paleta</Text>
+            </TouchableOpacity>
+
+
+        <Text style={styles.titulo}>Frango</Text>
+            <TouchableOpacity onPress={() => {
+              setIsEnabled(!isEnabled);
+              setID("7");       
+            }}>
+              <Text style={styles.btnTitle}>Coxa de frango</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => {
+              setIsEnabled(!isEnabled);
+              setID("8");       
+            }}>
+              <Text style={styles.btnTitle}>Asa de frango</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => {
+              setIsEnabled(!isEnabled);
+              setID("9");       
+            }}>
+              <Text style={styles.btnTitle}>Coração de frango</Text>
+            </TouchableOpacity>
+
+            {id && isEnabled ? <ModalReceita id={id} ativo={isEnabled}/> : ""}
     </View>
   );
 
